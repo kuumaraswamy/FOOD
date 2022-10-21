955cbcdbbba2cc2ecb40443419059de2bb61405f
kuumaraswamy <nkuumaraswamy@gmail.com>
2022-10-21 12:08:32 +0530

    Managing cart (close,open) by using useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 import Header from "./components/Layout/Header"
 import Meals from "./components/Meals/Meals"
 import Cart from "./components/Cart/Cart";
@@ -6,10 +6,20 @@ import Cart from "./components/Cart/Cart";
 
 
 function App() {
+     const [cartIsShown, setCartIsShown] = useState(false)
+
+    const showCartHandler = props =>{
+      setCartIsShown(true)
+    }
+    
+    const hideCartHandler = props =>{
+      setCartIsShown(false)
+    } 
+
   return (
     <Fragment>
-      <Cart/>
-      <Header />
+      { cartIsShown  &&  <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
